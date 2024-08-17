32ceae3ad6376a7f3f17b59f59b9d88e8353866e
Asad <asadullahkhan0098@gmail.com>
2024-08-18 00:10:24 +0530

    Render legal/disclaimer when its btn is clicked

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import './styles.css'
 import {getHomeContent} from './home.js';
 import { getMenuContent } from './menu.js';
+import { getLegalAndDislaimerContent } from './disclaimer.js';
 
 const contentDiv = document.querySelector('#content');
 const homeBtn = document.querySelector('button:nth-child(1)');
 const menuBtn = document.querySelector('button:nth-child(2)');
+const disclaimerBtn = document.querySelector('button:nth-child(3)');
 window.onload = function() {
     contentDiv.appendChild(getHomeContent().cardHeading);
     contentDiv.appendChild(getHomeContent().divCard);
@@ -27,4 +29,10 @@ menuBtn.addEventListener('click', () => {
     clearContentDiv();
     contentDiv.appendChild(getMenuContent().cardHeading);
     contentDiv.appendChild(getMenuContent().card);
+});
+
+disclaimerBtn.addEventListener('click', () => {
+    clearContentDiv();
+    contentDiv.appendChild(getLegalAndDislaimerContent().cardHeading);
+    contentDiv.appendChild(getLegalAndDislaimerContent().card);
 })
